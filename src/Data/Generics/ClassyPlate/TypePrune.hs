4ac0176c38dfa8195359fad053f41ebf6f36f7f8
nboldi <nboldi@elte.hu>
2016-12-10 21:40:05 +0100

    Somewhat optimized the calculation of ignoring subtrees but yet perfect.

--- src/Data/Generics/ClassyPlate/TypePrune.hs
+++ src/Data/Generics/ClassyPlate/TypePrune.hs
@@ -33,44 +33,32 @@ type family AnySelected (c :: * -> Constraint) (ls :: [*]) :: Bool where
   AnySelected c (fst ': rest) = AppSelector c fst || AnySelected c rest
   AnySelected c '[] = False
 
-type family MemberTypes (typ :: *) :: [*] where
-  MemberTypes t = GetMemberTypes '[] t
 
-type family GetMemberTypes (checked :: [*]) (typ :: *) :: [*] where
-  GetMemberTypes checked t = GetElementTypes t checked (Rep t)
+type MemberTypes t = MemberTypesAcc '[] '[t]
 
-type family GetElementTypes (t :: *) (checked :: [*]) (typ :: * -> *) :: [*] where 
-  GetElementTypes t checked (D1 md cons) = GetElementTypesCons t checked cons
+type family MemberTypesAcc (visited :: [*]) (open :: [*]) :: [*] where
+  MemberTypesAcc visited '[] = visited
+  MemberTypesAcc visited (open ': rest) = MemberTypesAcc (open ': visited) (Difference (open ': visited) (DirectMembers open))
 
-type family GetElementTypesCons (t :: *) (checked :: [*]) (typ :: * -> *) where 
-  GetElementTypesCons t checked (C1 (MetaCons consName pref flag) flds) = GetElementTypesFields consName 0 t checked flds
-  GetElementTypesCons t checked (c1 :+: c2) = GetElementTypesCons t checked c1 `Union` GetElementTypesCons t checked c2
+type family DirectMembers (typ :: *) :: [*] where
+  DirectMembers t = GetElementTypes t (Rep t)
 
-type family GetElementTypesFields (cons :: Symbol) (n :: Nat) (t :: *) (checked :: [*]) (typ :: * -> *) where 
-  GetElementTypesFields cons n t checked (fld1 :*: fld2) 
+type family GetElementTypes (t :: *) (typ :: * -> *) :: [*] where 
+  GetElementTypes t (D1 md cons) = GetElementTypesCons t cons
+
+type family GetElementTypesCons (t :: *) (typ :: * -> *) where 
+  GetElementTypesCons t (C1 (MetaCons consName pref flag) flds) = GetElementTypesFields consName 0 t flds
+  GetElementTypesCons t (c1 :+: c2) = GetElementTypesCons t c1 `Union` GetElementTypesCons t c2
+
+type family GetElementTypesFields (cons :: Symbol) (n :: Nat) (t :: *) (typ :: * -> *) where 
+  GetElementTypesFields cons n t (fld1 :*: fld2) 
     = -- only one field should be on the lhs
-      GetElementTypesFields cons n t checked fld1 `Union` GetElementTypesFields cons n t checked fld2
-  GetElementTypesFields cons n t checked (S1 (MetaSel fld unp str laz) (Rec0 innerT)) 
-    = WhenNotIgnoredElementTypes (IsIgnoredField cons n fld (IgnoredFields t)) checked innerT
-  GetElementTypesFields cons n t checked U1 = '[]
-
-type family WhenNotIgnoredElementTypes (ignored :: Bool) (checked :: [*]) (innerT :: *) :: [*] where
-  WhenNotIgnoredElementTypes False checked innerT = '[]
-  WhenNotIgnoredElementTypes True checked innerT = EvalGetElementTypesField checked innerT
-
-type family EvalGetElementTypesField checked innerT where
-  EvalGetElementTypesField checked innerT = GetElementTypesField checked (MyFind () innerT checked) innerT
+      GetElementTypesFields cons n t fld1 `Union` GetElementTypesFields cons n t fld2
+  GetElementTypesFields cons n t (S1 (MetaSel fld unp str laz) (Rec0 innerT)) 
+    = If (IsIgnoredField cons n fld (IgnoredFields t)) '[] '[innerT]
+  GetElementTypesFields cons n t U1 = '[]
 
 type family IsIgnoredField (cons :: Symbol) (fldNum :: Nat) (fldSelector :: Maybe Symbol)
                            (ignored :: [Either (Symbol, Nat) Symbol]) :: Bool where
   IsIgnoredField cons fldNum (Just sel) ignored = Find (Right sel) ignored || Find (Left '(cons, fldNum)) ignored
   IsIgnoredField cons fldNum Nothing ignored = Find (Left '(cons, fldNum)) ignored
-
-type family GetElementTypesField (checked :: [*]) (inChecked :: Bool) (typ :: *) where 
-  GetElementTypesField checked True typ = '[]
-  GetElementTypesField checked False typ = Insert typ (GetMemberTypes (typ ': checked) typ)
-
-type family MyFind info x ys where
-    MyFind info x '[]       = 'False
-    MyFind info x (x ': ys) = 'True
-    MyFind info x (y ': ys) = MyFind info x ys
